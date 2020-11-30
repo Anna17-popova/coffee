5b69a39d6f3b8312586eea0d069779c57263c2b9
user 2.0 <anna-062popova@yandex.ru>
2020-11-30 16:57:29 +0300

    кофе

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
+import sqlite3
 import sys
+
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
-from PyQt5.QtGui import QPainter, QColor
-from random import randrange
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        uic.loadUi('main.ui', self)
+        self.btn.clicked.connect(self.func)
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(['ID', 'название сорта',
+                                                                     'степень обжарки', 'молотый/в зернах',
+                                                                     'описание вкуса', 'цена', 'объем упаковки'])
+        self.tableWidget.resizeColumnsToContents()
 
-    def initUI(self):
-        self.setGeometry(300, 300, 600, 600)
-        self.btn = QPushButton('Начать', self)
-        self.btn.setGeometry(250, 250, 100, 30)
-        self.btn.clicked.connect(self.paint)
-        self.do_paint = False
-
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_ellips(qp)
-            qp.end()
-
-    def draw_ellips(self, qp):
-        qp.setBrush(QColor(randrange(0, 255), randrange(0, 255), randrange(0, 255)))
-        d = randrange(0, 300)
-        qp.drawEllipse(randrange(0, 500), randrange(0, 500), d, d)
+    def func(self):
+        name = self.name.text()
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        data = cur.execute(f'SELECT * FROM data WHERE name = ?', (name,)).fetchall()
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(data):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
 
 
 if __name__ == '__main__':
